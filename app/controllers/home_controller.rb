c2cc63cd2c4bca9f76c9f82bba4aba1a0450294d
yang <yang@yang.(none)>
2013-12-24 22:20:45 +0800

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+   def first
+  end
+   def second
+  end
+   def third
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Header::Application.config.secret_token = '081bb5b0f80a21f3c2a0fdb9911605da1b2eab9c6bb336cf867a65b4179b73d03eae15ad33e4f7a5c2efeabeb9b8001d97ce6a09bce8e48b3065647b10b62826'
